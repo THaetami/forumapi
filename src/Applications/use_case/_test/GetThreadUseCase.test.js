b4cc5da35ce630060b9609be9715f76a4b8d1b9b
Thaetami <tatanghaetami.97@gmail.com>
2023-03-29 10:00:55 +0700

    fixed getThreadUseCase testing #5

--- src/Applications/use_case/_test/GetThreadUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadUseCase.test.js
@@ -12,14 +12,13 @@ describe('GetThreadUseCase', () => {
     // Arrange
     const useCaseParams = 'thread-istInThreadCase';
 
-    const expectedDetailThread = new DetailThread({
+    const expectedDetailThread = {
       id: 'thread-istInThreadCase',
       title: 'thread title',
       body: 'thread body',
       date: '2023',
       username: 'John Doe',
-      comments: [],
-    });
+    };
 
     const retrievedComments = [
       new DetailComment({
@@ -29,16 +28,7 @@ describe('GetThreadUseCase', () => {
         content: 'comment A',
         replies: [],
         isDeleted: false,
-        likeCount: 0,
-      }),
-      new DetailComment({
-        id: 'comment-456',
-        username: 'user B',
-        date: '2020',
-        content: 'comment B',
-        replies: [],
-        isDeleted: false,
-        likeCount: 0,
+        likeCount: 2
       }),
     ];
 
@@ -51,23 +41,30 @@ describe('GetThreadUseCase', () => {
         username: 'user C',
         isDeleted: false,
       }),
-      new DetailReply({
-        id: 'reply-456',
-        commentId: 'comment-456',
-        content: 'reply B',
-        date: '2021',
-        username: 'user D',
-        isDeleted: false,
-      }),
     ];
 
-    const { commentId: commentIdReplyA, ...filteredReplyDetailsA } = retrievedReplies[0];
-    const { commentId: commentIdReplyB, ...filteredReplyDetailsB } = retrievedReplies[1];
-
-    const expectedCommentsAndReplies = [
-      { ...retrievedComments[0], replies: [filteredReplyDetailsA] },
-      { ...retrievedComments[1], replies: [filteredReplyDetailsB] },
-    ];
+    const expectedValue = {
+        id: 'thread-istInThreadCase',
+        title: 'thread title',
+        body: 'thread body',
+        date: '2023',
+        username: 'John Doe',
+        comments: [
+            {
+                id: 'comment-123',
+                username: 'user A',
+                date: '2021',
+                content: 'comment A',
+                likeCount: 2,
+                replies: [{
+                    id: 'reply-123',
+                    content: 'reply A',
+                    date: '2021',
+                    username: 'user C',
+                }],
+            }
+        ]
+    }
 
     // creating dependency of use cases
     const mockThreadRepository = new ThreadRepository();
@@ -83,7 +80,7 @@ describe('GetThreadUseCase', () => {
     mockReplyRepository.getReplyByThreadId = jest.fn()
       .mockImplementation(() => Promise.resolve(retrievedReplies));
     mockLikeCommentRepository.getCountLikeByCommentId = jest.fn()
-      .mockImplementation(() => Promise.resolve(0));
+      .mockImplementation(() => Promise.resolve(2));
 
     // creating use case instance
     const getThreadUseCase = new GetThreadUseCase({
@@ -94,11 +91,11 @@ describe('GetThreadUseCase', () => {
     });
 
     const getThread = await getThreadUseCase.execute(useCaseParams);
-     expect(getThread).toEqual(new DetailThread({
-      ...expectedDetailThread, comments: expectedCommentsAndReplies,
-    }));
+    expect(getThread).toEqual(expectedValue);
 
     expect(mockThreadRepository.getThreabById).toBeCalledWith(useCaseParams);
     expect(mockCommentRepository.getCommentByThreadId).toBeCalledWith(useCaseParams);
+    expect(mockReplyRepository.getReplyByThreadId).toBeCalledWith(useCaseParams);
+    expect(mockLikeCommentRepository.getCountLikeByCommentId('comment-123'));
   });
 });
